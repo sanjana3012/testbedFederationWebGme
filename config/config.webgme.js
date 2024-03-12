a80761b862d69093ba87d5525a08e3f77984e14b
sanjana3012 <sanjana.das@vanderbilt.edu>
2024-03-12 09:05:11 -0500

    added new seed

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -10,6 +10,7 @@ var config = require('webgme/config/config.default'),
 config.plugin.basePaths.push(__dirname + '/../src/plugins');
 config.seedProjects.basePaths.push(__dirname + '/../src/seeds/fabric_Seed');
 config.seedProjects.basePaths.push(__dirname + '/../src/seeds/fabric_seed_1');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/fabric_seed_2');
 
 
 
