d69faa62a73ed77c64ca0881b65c0997524b5cf3
Sanjana Das <sanjanadas@toors-MacBook-Pro-4.local>
2024-04-15 09:34:16 -0500

    docker

--- config/config.test.js
+++ config/config.test.js
@@ -6,6 +6,6 @@
 var config = require('./config.default');
 
 config.server.port = 9001;
-config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_tests';
+config.mongo.uri = 'mongodb://mongodb/webgmedb';
 
 module.exports = config;
